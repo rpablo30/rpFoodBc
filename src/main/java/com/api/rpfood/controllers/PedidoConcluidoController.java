a1c273fdcf9bd8ea40da49b0bec413ce32327412
Rodrigo Pablo <rpablo30@hotmail.com>
2023-12-17 09:33:28 -0300

    Criado o método no controller do PedidoConcluidoController para pegar tudo que tem na tabela de pedidosConcluidos e repassar para o frontend . Teste no postman ok .

--- src/main/java/com/api/rpfood/controllers/PedidoConcluidoController.java
+++ src/main/java/com/api/rpfood/controllers/PedidoConcluidoController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -33,6 +35,18 @@ public class PedidoConcluidoController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @GetMapping
+    public ResponseEntity<List<PedidoConcluido>> listarPedidosConcluidos() {
+        try {
+            List<PedidoConcluido> pedidosConcluidos = pedidoConcluidoService.listarTodosPedidosConcluidos();
+            return new ResponseEntity<>(pedidosConcluidos, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 
     @PostMapping("/enviar-relatorio")
     public ResponseEntity<String> enviarRelatorio(@RequestBody PedidoConcluido pedidoConcluido) {
